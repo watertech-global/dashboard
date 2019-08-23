e9a76ec0616cd0914f988a248b637ac6423935c0
Marcellin M. Wabo <5281242+mmarcwabo@users.noreply.github.com>
2019-08-23 02:10:12 +0100

    Add full version of billingController
    Add full version of billingController

--- controllers/billingController.js
+++ controllers/billingController.js
@@ -1,41 +1,46 @@
 var Billing = require('../models/Billing');
+var async = require('async');
+var Customer = require('../models/Customer');
 
 // Display list of all billings.
-exports.billing_list = function(req, res) {
-    res.send('NOT IMPLEMENTED: billing list');
+exports.billing_list = function(req, res, next) {
+  
+    Billing.find()
+    .sort([['_id', 'ascending']])
+    .exec(function (err, list_billings){
+        if(err){return next(err);}
+        //Else
+        //Success : means no error!
+        //send it on the page (view) 'billings'
+         res.send('billings', {title: 'Abonnements', billing_list: list_billings });
+      });
+   
 };
 
 // Display detail page for a specific billing.
-exports.billing_detail = function(req, res) {
-    res.send('NOT IMPLEMENTED: billing detail: ' + req.params.id);
-};
-
-// Display billing create form on GET.
-exports.billing_create_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: billing create GET');
-};
-
-// Handle billing create on POST.
-exports.billing_create_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: billing create POST');
-};
-
-// Display billing delete form on GET.
-exports.billing_delete_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: billing delete GET');
-};
-
-// Handle billing delete on POST.
-exports.billing_delete_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: billing delete POST');
-};
-
-// Display billing update form on GET.
-exports.billing_update_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: billing update GET');
-};
-
-// Handle billing update on POST.
-exports.billing_update_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: billing update POST');
+exports.billing_detail = function(req, res, next) {
+    
+    async.parallel({
+      billing: function(callback){
+          Billing.findById(req.params.id)
+          .exec(callback)
+      },
+      billings_customers : function(callback){
+        Customer.find({'billing': req.params.id}, 'Clients facturés')
+        .exec(callback)
+      }    },function(err, results){
+        if(err){return next(err);}//Error
+        if(results.billing ==null){//In case no results
+          var err = new Error('Compagnie non trouvée');
+          err.status = 404;
+          return next(err);
+        }
+        //Success ? so let's render it to the appropriate view
+        res.render('billings',
+                   {
+                    title: 'Abonnement du client',
+                    billing: results.billing,
+                    billing_customers: results.billings_customers,
+                    })
+      });
 };
