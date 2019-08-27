59e15381d98c2a9b5a8b790aa5784672307ffd64
Marcellin M. Wabo <5281242+mmarcwabo@users.noreply.github.com>
2019-08-27 13:57:58 +0100

    Upadte Billing

--- models/Billing.js
+++ models/Billing.js
@@ -5,11 +5,11 @@ const mongoose = require('mongoose');
 const Billing = new mongoose.Schema({
   idBilling: Number,
   billDateTime: { type: Date, default: Date.now },
-  Company: { type: mongoose.Schema.Types.ObjectId, ref: 'Company', required: true },
-  Customer: { type: mongoose.Schema.Types.ObjectId, ref: 'Customer', required: true },
+  company: { type: mongoose.Schema.Types.ObjectId, ref: 'Company', required: true },
+  customer: { type: mongoose.Schema.Types.ObjectId, ref: 'Customer', required: true },
   // To get meter SIM NUM
-  Meter: { type: mongoose.Schema.Types.ObjectId, ref: 'Meter', required: true },
-  Analysis: { type: mongoose.Schema.Types.ObjectId, ref: 'Analysis', required: true },
+  meter: { type: mongoose.Schema.Types.ObjectId, ref: 'Meter', required: true },
+  analysis: { type: mongoose.Schema.Types.ObjectId, ref: 'Analysis', required: true },
   // tarriffCode
 });
 module.exports = mongoose.model('Billing', Billing);
