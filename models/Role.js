6fd5ade0ed9e9b5871c7d11c7f24de0a7bb5dac7
Marcellin M. Wabo <5281242+mmarcwabo@users.noreply.github.com>
2019-08-27 13:54:28 +0100

    Update Designation/Role
    Update Designation/Role according to specs

--- models/Role.js
+++ models/Role.js
@@ -2,9 +2,8 @@
 //
 const mongoose = require('mongoose');
 // The Role
-
+//Made from the designation description in the specs documents
 const Role = new mongoose.Schema({
-  idRole: Number,
   roleCode: String,
   roleTitle: String,
   // The access lists here
