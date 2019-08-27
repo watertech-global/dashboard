955c5da87d1aca58f19df33ae52f29cab80242dd
Marcellin M. Wabo <5281242+mmarcwabo@users.noreply.github.com>
2019-08-27 13:45:52 +0100

    Update Worker

--- models/Worker.js
+++ models/Worker.js
@@ -7,8 +7,6 @@ const Worker = new mongoose.Schema({
   // here define all the entity
   // attributs according to its representation
   // in the DB
-
-  idWorker: { type: Number, default: 0 },
   User: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   Company: { type: mongoose.Schema.Types.ObjectId, ref: 'Company', required: true },
   // Worker Date of Join
