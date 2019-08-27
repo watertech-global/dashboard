3bdf2687d0a52d86979159b7fcca3c04f23163f3
Marcellin M. Wabo <5281242+mmarcwabo@users.noreply.github.com>
2019-08-27 13:46:37 +0100

    Update Country

--- models/Country.js
+++ models/Country.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const Country = new mongoose.Schema({
-  idCountry: { type: Number, default: '0' },
   countryName: { type: String, default: 'DR Congo' },
   countryCode: { type: String, default: 'CD' },
+  countryOther : String
 });
 module.exports = mongoose.model('Country', Country);
